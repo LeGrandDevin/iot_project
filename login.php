79efc593fcaabfb9ef02feb26ab68272ed6d4060
LeGrandDevin <51027695+LeGrandDevin@users.noreply.github.com>
2022-06-26 20:06:47 +0200

    add graph

--- login.php
+++ login.php
@@ -24,6 +24,13 @@
             <label for="lastname" class="placeholder">Mot de passe</label>
         </div>
 
+        <div id="credResult">
+            <?php
+            session_start();
+            echo(session_id());
+            verifyCred();
+            ?>
+        </div>
         <!-- <input type="submit" value="log-in"> -->
         
         <input type="submit" class="submit" value="log-in">
@@ -32,45 +39,55 @@
 </div>
 
 
+
 <?php 
 
-verifyCred();
 
-function checkSession(){
-
-    if (session_id() == '' || !isset($_SESSION['login'])) { //if sid exists and login for sid exists
-
-    }else{
-        echo "Hi, " . $_SESSION['login'];
-    }
+function checkSession() : bool{
+    return session_id() == '' || !isset($_SESSION['login']);
 }
 
 
 function verifyCred(){
 
     
-    if (isset($_POST['login']) && isset($_POST['password'])) //when form submitted
+    if (isset($_POST['login']) && isset($_POST['password'])){ //when form submitted
         $formLogin = $_POST['login']; 
         $formPassword = $_POST['password'];
         $db = new PDO ("mysql:host=185.224.138.133;dbname=u928306449_groupe_quatre;charset=utf8","u928306449_groupe_quatre","KeyceGroupe4");
         $req = $db->prepare('SELECT * FROM credentials WHERE identifiant = "' .  $formLogin . '" AND password = "' . $formPassword . '"');
         $req->execute();
         $id = $req->fetchAll();
+        if(sizeof($id) < 1){
+            echo("Mauvais identifiant ou mots de passe.");
+            return;
+        }
         $bddLogin = $id[0][1];
         $bddpassword = $id[0][2];
-    {
-    if ($formLogin === $bddLogin && $formPassword === $bddpassword)
-    
-    {
-        $_SESSION['login'] = $formLogin; //write login to server storage
-        checkSession();
-        //header('Location: /iot/login.php'); //redirect to main
-    }
-    else
-    {
-        echo "<script>alert('Wrong login or password');</script>";
-        echo "<noscript>Wrong login or password</noscript>";
-    }
+        
+        if ($formLogin === $bddLogin && $formPassword === $bddpassword)
+        {   
+            if(checkSession()){
+                $_SESSION['login'] = $formLogin; //write login to server storage
+                $_COOKIE['sesid'] = session_id();
+                echo("session started");
+            }
+            header('Location: /iot_project/dashboard.php'); //redirect to main
+        }
     }
 
 }
+
+?>
+
+<div style="color: white;">
+<?php
+    if(checkSession()){
+        var_dump($_SESSION);
+    }else{
+        echo ("pas de sesion");
+    }
+?>
+</div>
+
+
