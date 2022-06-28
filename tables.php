635edf989c654e5b2898066a5942710448655710
DamienAmalou <damien.amalou@gmail.com>
2022-06-28 23:43:00 +0200

    front table php
    tables.css, base de donnée en table dans table.php

--- tables.php
+++ tables.php
@@ -13,94 +13,30 @@ $data = $req->fetchAll();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="dashboard.css" />
-    <title>DashBoard</title>
+    <link rel="stylesheet" href="tables.css" />
+    <title>Tables</title>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <!-- -->
     <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
 </head>
 
-<body class="sb-nav-fixed">
+<body>
 
-<nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-    <!-- Navbar Brand-->
-    <a class="navbar-brand ps-3" href="dashboard.php">IOT Project</a>
-    <!-- Navbar-->
-    <ul class="navbar-nav ms-auto me-lg-4">
-        <li class="dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#!">Déconnexion</a></li>
-            </ul>
-        </li>
-    </ul>
-</nav>
-<div id="layoutSidenav">
-    <div id="layoutSidenav_nav">
-        <nav class="sb-sidenav sb-sidenav-dark" id="sidenavAccordion">
-            <div class="sb-sidenav-menu">
-                <div class="nav">
-                    <div class="sb-sidenav-menu-heading">Accueil</div>
-                    <a class="nav-link" href="dashboard.php">
-                        <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                        Dashboard
-                    </a>
-                    <div class="sb-sidenav-menu-heading">Fonctionnalités</div>
-                    <a class="nav-link" href="graphs.php">
-                        <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-                        Graphiques
-                    </a>
-                    <a class="nav-link" href="tables.php">
-                        <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                        Tables
-                    </a>
-                </div>
-            </div>
-
-        </nav>
-    </div>
-
+<div class="tables">
+    <?php
+    $conn = mysqli_connect("185.224.138.133", "u928306449_groupe_quatre","KeyceGroupe4","u928306449_groupe_quatre" );
+    $sql = mysqli_query($conn, "SELECT * FROM captorData" );
+    echo '<table class="table-content">';
+    echo '<tr class="table-header">'.'<td class="header-item">idcaptorData</td>'.'<td class="header-item">humidity</td>'.'<td class="header-item">temperature</td>'.'<td class="header-item">date</td>';
+    while($data = mysqli_fetch_array($sql))
+    {
+        echo '<tr>'.'<td>'.$data['idcaptorData'].'</td>'.'<td>'.$data['humidity'].'</td>'.'<td>'.$data['temperature'].'</td>'.'<td>'.$data['date'].'</td>'.'</tr>'/*."<br/>"*/ ;
+    }
+    echo '</table>';
+    ?>
 </div>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 <script src="js/scripts.js"></script>
 
-<?php
-session_start();
-?>
-<div>
-    <canvas id="myChart"></canvas>
-</div>
 </body>
 </html>
-
-<script>
-    const labels = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-    ];
-
-    const data = {
-        labels: labels,
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45],
-        }]
-    };
-
-    const config = {
-        type: 'line',
-        data: data,
-        options: {}
-    };
-
-    const myChart = new Chart(
-        document.getElementById('myChart'),
-        config
-    );
-</script>
