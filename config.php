8bd9556d1d0b2f6f12e32020a2adcc206a44a3b5
LeGrandDevin <51027695+LeGrandDevin@users.noreply.github.com>
2022-06-30 18:13:12 +0200

    db access to conf.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+    define('DB_TYPE', 'mysql');
+    define('DB_HOST', '185.224.138.133');
+    define('DB_NAME', 'u928306449_groupe_quatre');
+    define('DB_USERNAME', 'u928306449_groupe_quatre');
+    define('DB_PASSWORD', 'KeyceGroupe4');
+?>
--- tableBack.php
+++ tableBack.php
@@ -1,12 +1,15 @@
 
 <div class="tables" id="tables">
     <?php
+    require_once 'toolBox.php';
+    $allData = getCaptorAllValue(date("y-m-d h:i:s",0), date("y-m-d h:i:s",time() + 14 * 60 * 60 ));
     $conn = mysqli_connect("185.224.138.133", "u928306449_groupe_quatre","KeyceGroupe4","u928306449_groupe_quatre" );
     $sql = mysqli_query($conn, "SELECT * FROM captorData" );
+
     echo '<table class="table-content" id="table-content">';
     echo '<thead><tr class="table-header">'.'<td class="header-item">idcaptorData</td>'.'<td class="header-item">humidity</td>'.'<td class="header-item">temperature</td>'.'<td class="header-item">date</td></thead>';
     echo '<tbody>';
-    while($data = mysqli_fetch_array($sql))
+    foreach($allData as $data)
     {
         echo '<tr class="table-row">'.'<td class="table-data">'.$data['idcaptorData'].'</td>'.'<td class="table-data">'.$data['humidity'].'</td>'.'<td class="table-data">'.$data['temperature'].'</td>'.'<td class="table-data">'.$data['date'].'</td>'.'</tr>'/*."<br/>"*/ ;
     }
